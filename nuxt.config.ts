01e17b9d8370899559ef037331f1f6770727dc17
antonio <antonio.medeiros@estudante.ufjf.br>
2022-06-12 18:58:51 -0300

    incluindo link para wpp e titulo da pagina

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,13 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: 'static',
+    meta: {
+        title: 'Antonio Dev',
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxtjs/google-fonts',
         '@nuxt-hero-icons/outline/nuxt',
         '@nuxt-hero-icons/solid/nuxt',
     ],
-    purgeCSS: {
-        whitelist: ['lvml'],
-        whitelistPatterns: [/leaflet-.+$/],
-        whitelistPatternsChildren: [/leaflet-.+$/]
-    },
 })
