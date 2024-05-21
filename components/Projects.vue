65bd6d92088a41310909cbeaa83d73200ec07020
Antonio <antonio.medeiros@estudante.ufjf.br>
2024-05-21 15:29:40 -0300

    fix filter

--- components/Projects.vue
+++ components/Projects.vue
@@ -55,7 +55,7 @@ const clearSelectedTags = () => {
       class=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
     >
       <Card
-        v-for="repo in repoData.filter(repo => repo.topics.some(tag => allTags[tag]))"
+        v-for="repo in repoData.filter(repo => Object.keys(allTags).filter(tag => allTags[tag]).every(tag => repo.topics.includes(tag)))"
         :key="repo.name"
         :title="repo.name"
         :descricao="repo.description"
